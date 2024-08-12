f59387939727a13bd3ac7034d3b32c034b932908
[mrugank raut] <[raut21bcs39@iiitkottayam.ac.in]>
2024-08-12 11:58:05 +0530

    Initial Upload

---
+++ ecom-proj/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, description, brand, price, category, release_date, product_available, stock_quantity)
+VALUES ('Tata Nexon', 'A compact SUV with excellent safety features and performance.', 'Tata Motors', 750000.00, 'Cars',
+        '2024-01-15', true, 50);
